96d2ab045ad7fcdbe0b4a464d9bfad220ccea143
talalriaz <ahmadtalalz@yahoo.com>
2025-11-03 15:31:26 +0500

    removed files

--- test_vector_deployment.py
+++
@@ -1,6 +0,0 @@
-from src.retrieval.vector_db import VectorDBManager
-
-query_text = "What is the policy for sick leave?"
-vector_db_manager = VectorDBManager()
-text = vector_db_manager.query_db(query_text,top_k=3)
-print(text)
